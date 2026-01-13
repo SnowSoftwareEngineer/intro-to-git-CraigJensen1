d304e05657bbe60219eabef23ee86be4f94a683d
Craig Jensen1 <craig.jensen1@snow.edu>
2026-01-13 14:02:24 -0700

    My cool commit

--- ConsoleAppEmpty/Program.cs
+++ ConsoleAppEmpty/Program.cs
@@ -4,5 +4,25 @@
     {
         Console.WriteLine("Hello World");
         Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
     }
 }
